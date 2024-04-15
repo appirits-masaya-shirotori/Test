076a1ba06595ac45840e057f5a4847da47631fb3
appirits-masaya-shirotori <163282437+appirits-masaya-shirotori@users.noreply.github.com>
2024-04-16 08:52:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
-  resources :users, only: [:new, :create, :index, :destroy, :show]
-  resources :item_masters, only: [:index, :new, :create, :show, :edit, :destroy]
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :users do
-      resources :items, only: [:index]
-    end
-    get 'select_user', to: 'items#select_user'
+  resources :users, only: [:new, :create, :index, :destroy] do
+    resources :items, only: [:index]
+  end
+  resources :items, only: [:index, :new, :create, :show]
+  resources :item_masters, only: [:index, :new, :create, :show]
+  get 'select_user', to: 'items#select_user'
 end
