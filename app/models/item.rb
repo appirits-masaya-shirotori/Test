9891d40ba157ade723782b849137aa027b96e786
appirits-masaya-shirotori <163282437+appirits-masaya-shirotori@users.noreply.github.com>
2024-04-16 09:14:54 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,18 +2,21 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :item_master
 
-  before_create :merge_with_existing_item
+  before_save :merge_with_existing_item
   after_save :remove_if_zero_quantity
   delegate :id, :name, :description, to: :item_master, prefix: true
 
   private
 
   def merge_with_existing_item
-    existing_item = Item.find_by(user_id: user_id, item_master_id: item_master_id)
-    if existing_item
-      existing_item.quantity += self.quantity
-      existing_item.save
-      self.quantity = 0 # このアイテムの数量を0に設定して保存を防ぐ
+    return unless new_record?
+
+    Item.transaction do
+      existing_item = Item.lock.find_by(user_id: user_id, item_master_id: item_master_id)
+      if existing_item
+        existing_item.increment!(:quantity, self.quantity)
+        self.quantity = 0
+      end
     end
   end
 
